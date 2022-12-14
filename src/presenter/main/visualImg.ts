55bdbeaf54dff438a5c6c2335898fadbb3148dd2
kim eunhye <117132422+uuuuooi@users.noreply.github.com>
2022-12-14 19:02:26 +0900

    data 수정

--- src/presenter/main/visualImg.ts
+++ src/presenter/main/visualImg.ts
@@ -1,18 +1,38 @@
 export const VisualImage = [
   {
     id: 1,
+    title: "",
+    desc: "",
     images:
-      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/b434f117412b451694ece32b5e38c7ff.jpg",
+      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/40eb66d99a0c47419c405b0a471307e4.jpg",
   },
   {
     id: 2,
+    title: "",
+    desc: "",
     images:
-      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/a782702a12dc4f9daf1c06a0c288d360.jpg",
+      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/a744b994004242869f0ad158cbc00988.jpg",
   },
 
   {
     id: 3,
+    title: "",
+    desc: "",
     images:
-      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/88ab8ee909d3438daf7dfaeea025d58c.jpg",
+      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/b434f117412b451694ece32b5e38c7ff.jpg",
+  },
+  {
+    id: 4,
+    title: "",
+    desc: "",
+    images:
+      " https://woowahan-cdn.woowahan.com/new_resources/image/banner/5b4f8059ae0e46b690d2cdd4b194a1a8.jpg",
+  },
+  {
+    id: 5,
+    title: "",
+    desc: "",
+    images:
+      "https://woowahan-cdn.woowahan.com/new_resources/image/banner/21d6e2194ef542c7a80628281c0c2228.jpg",
   },
 ];
